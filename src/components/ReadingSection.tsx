8b6df030b25ec8268b47af516ab4a5abcb62e18e
Jon Galante <jonmgalante@gmail.com>
2026-01-10 17:41:42 -0500

    Refactor ReadingSection component and update link

--- src/components/ReadingSection.tsx
+++ src/components/ReadingSection.tsx
@@ -1,26 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
-import React from 'react';
-
-interface BookData {
-  name: string;
-  author: string;
-  link?: string;
-}
-
-interface ReadingSectionProps {
-  books?: BookData[];
-}
-
-const ReadingSection: React.FC<ReadingSectionProps> = () => {
+const ReadingSection: React.FC = () => {
   return (
     <section className="section mt-8">
       <h2 className="text-xl font-serif mb-4">Reading</h2>
-      <p>My <a 
-        href="https://docs.google.com/document/d/1RvKbNU5XghYzoVPLrRQl57a4t4yunfApmUOAYmyqPCg/edit?usp=sharing" 
-        target="_blank" 
-        rel="noopener noreferrer" 
-        className="text-blue-600 hover:text-blue-800 underline"
-      >library</a>.</p>
+      <p>
+        My{" "}
+        <Link
+          to="/reading"
+          className="text-blue-600 hover:text-blue-800 underline"
+        >
+          library
+        </Link>
+        .
+      </p>
     </section>
   );
 };
